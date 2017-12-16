eb1183de5c98cff5b1c240bea2d72405bbc7c997
GummyVites <kemalee@ucsc.edu>
2017-12-16 01:28:50 -0800

    Search Bar Works

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,20 +1,44 @@
 <template>
   <div class = "test">
-  <div v-for ="n in 100" class="card card-1">
+  <div class="search-wrapper">
+    <input type="text" v-model="search" placeholder="Search Coin Symbol"/>
+  </div>
+  <div v-if = "search !='' " v-for ="item in filteredName" icon="search" class="card card-1">
+    <div>{{item.name}}</div>
+    <div>{{item.symbol}}</div>
+    <div>Coin Price in USD: ${{item.price_usd}}</div>
+    <div>Bitcoin Price: {{item.price_btc}}</div>
+    <div>Market Cap: {{item.market_cap_usd}}</div>
+    <div>One Hour Change: {{item.percent_change_1h}}</div>
+    <div>One Day Change: {{item.percent_change_7d}}</div>
+    <div>One Week Change: {{item.percent_change_24h}}</div>
+  </div>
+
+  <div v-else v-for ="n in 100" class="card card-1">
     <div>{{coinNames[n-1]}}</div>
     <div>{{coinSymbol[n-1]}}</div>
-    <div>Coin Price in USD: {{coinPrice[n-1]}}</div>
+    <div>Coin Price in USD: ${{coinPrice[n-1]}}</div>
+    <div>Bitcoin Price: {{bitcoinPrice[n-1]}}</div>
     <div>Market Cap: {{marketCap[n-1]}}</div>
     <div>One Hour Change: {{change1hour[n-1]}}</div>
     <div>One Day Change: {{change24hour[n-1]}}</div>
     <div>One Week Change: {{change1week[n-1]}}</div>
   </div>
+
 </div>
 </template>
 
 <script>
   export default {
     name: 'test',
+    computed:{
+      filteredName:function(){
+          return this.coins.filter((item) => {
+             return item.symbol.toUpperCase().includes(this.search.toUpperCase());
+          });
+          // }
+        }
+      },
     data() {
       return {
         coinNames: [],
@@ -28,31 +52,34 @@
         change1week: [],
         coinImg: [],
         maxSupply: [],
-        totalSuply: []
+        totalSuply: [],
+        coins: [],
+        search: ''
       }
     },
-    created: function() {
+      created: function() {
       this.$http.get('https://api.coinmarketcap.com/v1/ticker/')
         .then(function(response) {
           var datas = response.data;
-          // console.log(datas);
+          this.coins = response.data;
+          // console.log(this.coins);
+
           for (var i = 0; i < Object.keys(datas).length; i++) {
-            this.coinNames[i] = datas[i].name;
-            this.coinSymbol[i] = datas[i].symbol;
-            this.rank[i] = datas[i].rank;
-            this.coinPrice[i] = datas[i].price_usd;
-            this.bitcoinPrice[i] = datas[i].price_btc;
-            this.marketCap[i] = datas[i].market_cap_usd;
-            this.change1hour[i] = datas[i].percent_change_1h;
-            this.change24hour[i] = datas[i].percent_change_7d;
-            this.change1week[i] = datas[i].percent_change_24h;
-            this.totalSuply[i] = datas[i].total_supply;
-            this.maxSupply[i] = datas[i].max_supply;
-            // console.log(this.coinNames[i]);
+            this.$set(this.coinNames, i, datas[i].name)
+            this.$set(this.coinSymbol, i, datas[i].symbol)
+            this.$set(this.rank, i, datas[i].rank)
+            this.$set(this.coinPrice, i, datas[i].price_usd)
+            this.$set(this.bitcoinPrice, i, datas[i].price_btc)
+            this.$set(this.marketCap, i, datas[i].market_cap_usd)
+            this.$set(this.change1hour, i, datas[i].percent_change_1h)
+            this.$set(this.change24hour, i, datas[i].percent_change_7d)
+            this.$set(this.change1week, i, datas[i].percent_change_24h)
+            this.$set(this.totalSuply, i, datas[i].total_supply)
+            this.$set(this.maxSupply, i, datas[i].max_supply)
           }
         })
     }
-  }
+    }
 </script>
 
 <style scoped>
@@ -75,4 +102,14 @@
   .card-1:hover {
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
   }
+
+  .search-wrapper{
+    margin: 10px 0;
+  }
+
+  input {
+    width: 400px;
+    height: 20px;
+
+}
 </style>
